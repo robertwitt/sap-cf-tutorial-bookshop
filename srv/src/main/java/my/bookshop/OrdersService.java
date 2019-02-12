579219416de1ee6f2fbda0ca588fe8f4bca5929f
Robert Witt <robert.witt.rw@gmail.com>
2019-02-12 11:38:30 +0000

    Add persistence via JPA.

--- srv/src/main/java/my/bookshop/OrdersService.java
+++ srv/src/main/java/my/bookshop/OrdersService.java
@@ -1,14 +1,23 @@
 package my.bookshop;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
 
 import com.sap.cloud.sdk.service.prov.api.*;
 import com.sap.cloud.sdk.service.prov.api.annotations.*;
 import com.sap.cloud.sdk.service.prov.api.exits.*;
+import com.sap.cloud.sdk.service.prov.api.operations.*;
 import com.sap.cloud.sdk.service.prov.api.request.*;
 import com.sap.cloud.sdk.service.prov.api.response.*;
+import com.sap.demo.bookshop.jpa.my.bookshop.*;
 
+import org.apache.olingo.odata2.api.exception.ODataApplicationException;
 import org.slf4j.*;
 
 public class OrdersService {
@@ -35,6 +44,41 @@ public class OrdersService {
 				.map(ed -> EntityData.getBuilder(ed).addElement("amount", 1000).buildEntityData("Orders"))
 				.collect(Collectors.toList());
 		return QueryResponse.setSuccess().setData(modifiedList).response();
+	}
+	
+	@Create(entity = "Orders", serviceName="CatalogService")
+	public CreateResponse createOrder(CreateRequest createRequest, ExtensionHelper h)
+			throws NamingException, ODataApplicationException {
+		// First, we fetch an EntityManager from JNDI context to perform our JPA
+		// operations with.
+		EntityManager em = (EntityManager) new InitialContext().lookup("java:comp/env/jpa/default/pc");
+
+		// Next, we create an Orders object from the data sent with the Create request.
+		// We set an ID for the order, its creation time, an order amount of 1000 units,
+		// and set the object reference to the ordered book. All other attributes of the
+		// Orders entity remain unchanged (for example, the buyer).
+		Orders order = createRequest.getData().as(Orders.class);
+		order.setID(UUID.randomUUID().toString());
+		order.setDate(new Date());
+		order.setAmount(1000);
+		order.setBook(em.find(Books.class, createRequest.getData().getElementValue("book_ID")));
+
+		// Using the persist method of the EntityManager we add the newly created
+		// Orders object to the JPA persistency, so that once the current transaction is
+		// committed, this new order is added to the database.
+		em.persist(order);
+
+		// Finally, to return the newly created entity in the response, we create an
+		// EntityData representation of the order.
+		EntityData createdEntity = EntityData.getBuilder(createRequest.getData())
+				.addElement("ID", order.getID())
+				.addElement("book", order.getBook())
+				.addElement("buyer", order.getBuyer())
+				.addElement("date", order.getDate())
+				.addElement("amount", order.getAmount())
+				.buildEntityData("Orders");
+		
+		return CreateResponse.setSuccess().setData(createdEntity).response();
 	}
 
 }
