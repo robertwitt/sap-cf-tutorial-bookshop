505ce3ac5a529b9e05b770e8af33f060aa1d5fa8
Robert Witt <robert.witt.rw@gmail.com>
2019-02-12 09:41:44 +0000

    Add UI module to business application.

---
+++ app/webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("de.robertwitt.tutorial.bookshop.app.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("de.robertwitt.tutorial.bookshop.app.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
